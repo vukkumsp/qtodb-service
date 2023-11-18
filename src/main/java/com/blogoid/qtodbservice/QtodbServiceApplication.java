b2826f5b09c3b10f937a215b66c5a0fcaa592f00
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2023-11-18 19:34:16 +0530

    Initial commit

---
+++ src/main/java/com/blogoid/qtodbservice/QtodbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blogoid.qtodbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtodbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QtodbServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogoid/qtodbservice/QtodbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogoid.qtodbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QtodbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
